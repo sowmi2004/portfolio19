c2762a607949d1c91062165042c9d9f5c2633640
sowmiya <sowmiyadevi@kaspontech.com>
2025-03-13 13:59:22 +0530

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://sowmi2004.github.io/portfolio19',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
