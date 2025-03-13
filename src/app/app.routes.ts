45670a8dc2a8a81f86b7126b8a252998787d19a9
sowmiya <sowmiyadevi@kaspontech.com>
2025-03-13 16:04:32 +0530

    components created and routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+   { path : '', component: HomeComponent},     // redirection to home component 
+   { path : 'about', component: AboutComponent},
+   { path : 'skills', component: SkillsComponent},
+   { path : 'projects', component: ProjectsComponent},
+   { path : 'contact', component: ContactComponent},
+   { path : '**', redirectTo : '' , pathMatch: 'full'}                    //last one is always a wild card route.
+
+];
+
+@NgModule({
+   imports : [RouterModule.forRoot(routes)],
+   exports : [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ src/app/components/redirect/redirect.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redirect',
+  imports: [],
+  templateUrl: './redirect.component.html',
+  styleUrl: './redirect.component.scss'
+})
+export class RedirectComponent {
+
+}
